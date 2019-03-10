e065ab4a586de26c387ef0e602818949e68fc756
John Tower <juan.c.torres1140@gmail.com>
2019-03-09 22:49:03 -0500

    Html layout

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCFjwY3uvJYXuWuPczMjHdM_woeceSiGAk",
+    authDomain: "train-station-3f870.firebaseapp.com",
+    databaseURL: "https://train-station-3f870.firebaseio.com",
+    projectId: "train-station-3f870",
+    storageBucket: "train-station-3f870.appspot.com",
+    messagingSenderId: "663553491494"
+};
+firebase.initializeApp(config);
