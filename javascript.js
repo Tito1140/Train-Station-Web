cf0513b8e60caad45bec23546bb45411f5fed4d8
John Tower <juan.c.torres1140@gmail.com>
2019-03-10 23:44:47 -0400

    rough tables

--- javascript.js
+++ javascript.js
@@ -6,4 +6,30 @@ var config = {
     storageBucket: "train-station-3f870.appspot.com",
     messagingSenderId: "663553491494"
 };
+
 firebase.initializeApp(config);
+
+var dataRef = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstTrain = 0;
+var frequency = 0;
+
+$("#add-train").on("click", function (event) {
+    event.preventDefault();
+
+    trainName = $("#name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    firstTrain = $("#departure-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+
+    dataRef.ref().push({
+
+        trainName: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+    });
+});
+
